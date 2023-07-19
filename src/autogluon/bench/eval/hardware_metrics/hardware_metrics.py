af35738a1658e073bb83eb09d16b7e2086ed13b2
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-07-19 15:32:24 -0700

    hardware_metrics script

--- src/autogluon/bench/eval/hardware_metrics/hardware_metrics.py
+++ src/autogluon/bench/eval/hardware_metrics/hardware_metrics.py
@@ -1,49 +1,47 @@
 import csv
 import logging
+import os
 from datetime import datetime, timedelta
 from typing import List, Optional
 
 import boto3
-import os
+import pandas as pd
 import typer
 import yaml
 
 cloudwatch_client = None
 metrics_list = []
+aws_account_id = None
+aws_account_region = None
 
 
-def get_instance_ids(config_file: str):
-    if not config_file:
-        raise ValueError("Invalid Config File")
-    logger.info(f"Getting hardware metrics for jobs under config file: {config_file}")
-    with open(config_file, "r") as f:
-        config = yaml.safe_load(f)
-    aws_account_id = config.get("CDK_DEPLOY_ACCOUNT")
-    aws_account_region = config.get("CDK_DEPLOY_REGION")
-    job_ids = list(config.get("job_configs", {}).keys())
+def get_job_ids(config_file: str):
+    job_ids = list(config_file.get("job_configs", {}).keys())
+    return job_ids
+
+
+def get_instance_id(job_id):
     batch_client = boto3.client("batch", region_name=f"{aws_account_region}")
     ecs_client = boto3.client("ecs", region_name=f"{aws_account_region}")
-    instance_ids = []
-    cluster_arns = []
-    container_arns = []
-    for job_id in job_ids:
-        response = batch_client.describe_jobs(jobs=[f"{job_id}"])
-        if response:
-            container_arn = response["jobs"][0]["container"]["containerInstanceArn"]
-            container_arns.append(container_arn)
-            cluster_arn = response["jobs"][0]["container"]["taskArn"].split("/")
-            cluster = f"arn:aws:ecs:{aws_account_region}:{aws_account_id}:cluster/" + cluster_arn[1]
-            cluster_arns.append(cluster)
-    for i in range(len(cluster_arns)):
-        response = ecs_client.describe_container_instances(
-            cluster=cluster_arns[i], containerInstances=[container_arns[i]]
-        )
-        instance_id = response["containerInstances"][0]["ec2InstanceId"]
-        instance_ids.append(instance_id)
-    return instance_ids
+
+    response = batch_client.describe_jobs(jobs=[f"{job_id}"])
+    if response:
+        container_arn = response["jobs"][0]["container"]["containerInstanceArn"]
+        cluster_arn = response["jobs"][0]["container"]["taskArn"].split("/")
+        cluster = f"arn:aws:ecs:{aws_account_region}:{aws_account_id}:cluster/" + cluster_arn[1]
+
+    response = ecs_client.describe_container_instances(cluster=cluster, containerInstances=[container_arn])
+    instance_id = response["containerInstances"][0]["ec2InstanceId"]
+    return instance_id
 
 
-def get_instance_util(instance_id: str, metric: str, statistics: Optional[List[str]] = ["Average"]) -> dict:
+def get_instance_util(
+    instance_id: str,
+    metric: str,
+    start_time: datetime,
+    end_time: datetime,
+    statistics: Optional[List[str]] = ["Average"],
+) -> dict:
     return cloudwatch_client.get_metric_statistics(
         Namespace="AWS/EC2",
         MetricName=metric,
@@ -51,29 +49,38 @@ def get_instance_util(instance_id: str, metric: str, statistics: Optional[List[s
             {"Name": "InstanceId", "Value": instance_id},
         ],
         Statistics=statistics,
-        StartTime=datetime.now() - timedelta(hours=1),
-        EndTime=datetime.now(),
+        StartTime=start_time,
+        EndTime=end_time,
         Period=120,
     )
 
 
 def results_to_csv():
-    csv_headers = ["InstanceID", "Metric", "Timestamp", "Statistic", "Unit"]
+    csv_headers = ["Framework", "Dataset", "Metric", "Timestamp", "Statistic", "Unit"]
     file_dir = os.path.dirname(__file__)
-    csv_location =  os.path.join(file_dir, "hardware_metrics.csv")
+    csv_location = os.path.join(file_dir, "hardware_metrics.csv")
     with open(csv_location, "w", newline="") as csvFile:
         writer = csv.DictWriter(csvFile, fieldnames=csv_headers)
         writer.writeheader()
         writer.writerows(metrics_list)
 
 
-def format_metrics(instance_metrics: dict, instance_id: str, statistics: Optional[List[str]] = ["Average"]):
+def format_metrics(
+    instance_metrics: dict,
+    framework: str,
+    dataset: str,
+    mode: str,
+    fold: int,
+    statistics: Optional[List[str]] = ["Average"],
+):
     output_dict = {}
-    output_dict["InstanceID"] = instance_id
-    output_dict["Metric"] = instance_metrics["Label"]
+    output_dict["framework"] = framework
+    output_dict["dataset"] = dataset
+    output_dict["mode"] = mode
+    output_dict["fold"] = fold
     for i in range(len(instance_metrics["Datapoints"])):
         for stat in statistics:
-            try:    
+            try:
                 output_dict["Timestamp"].append(
                     instance_metrics["Datapoints"][i]["Timestamp"].strftime("%m/%d/%Y: %H:%M:%S")
                 )
@@ -87,31 +94,67 @@ def format_metrics(instance_metrics: dict, instance_id: str, statistics: Optiona
     return output_dict
 
 
+def get_metrics(
+    job_id: str,
+    metrics: list,
+    s3_bucket: str,
+    module: str,
+    benchmark_name: str,
+    sub_folder: str,
+):
+
+    result_path = f"{module}/{benchmark_name}/{sub_folder}"
+    path_prefix = f"s3://{s3_bucket}/{result_path}"
+    global metrics_list
+    instance_id = get_instance_id(job_id)
+    s3_path_to_csv = f"{path_prefix}/results.csv"
+    results = pd.read_csv(s3_path_to_csv)
+    for metric in metrics:
+        for i in results.index:
+            framework, dataset, utc, train_time, predict_time, fold = (
+                results["framework"][i],
+                results["task"][i],
+                results["utc"][i],
+                results["training_duration"][i],
+                results["predict_duration"][i],
+                results["fold"][i],
+            )
+            training_util = get_instance_util(instance_id, f"{metric}", utc, train_time)
+            predict_util = get_instance_util(instance_id, f"{metric}", utc, train_time + predict_time)
+            metrics_list.append(format_metrics(training_util, framework, dataset, fold, "Training"))
+            metrics_list.append(format_metrics(predict_util, framework, dataset, fold, "Prediction"))
+
+
 def get_hardware_metrics(
     config_file: str = typer.Option(None, "--config-file", help="Path to YAML config file containing job ids."),
+    s3_bucket: str = typer.Argument(help="Name of the S3 bucket to which the aggregated results will be outputted."),
+    module: str = typer.Argument(help="Can be one of ['tabular', 'multimodal']."),
+    benchmark_name: str = typer.Argument(
+        help="Folder name of benchmark run in which all objects with path 'scores/results.csv' get aggregated."
+    ),
 ):
-    instance_ids = get_instance_ids(config_file)
-    global cloudwatch_client
-    cloudwatch_client = boto3.client("cloudwatch", region_name="us-east-2")
-    global metrics_list
-    for instance_id in instance_ids:
-        cpu_util = get_instance_util(instance_id, "CPUUtilization")
-        metrics_list.append(format_metrics(cpu_util, instance_id))
+    if not config_file:
+        raise ValueError("Invalid Config File")
+    logger.info(f"Getting hardware metrics for jobs under config file: {config_file}")
+    with open(config_file, "r") as f:
+        config = yaml.safe_load(f)
+    job_ids = get_job_ids(config)
 
-        ebs_write_ops = get_instance_util(instance_id, "EBSWriteOps")
-        metrics_list.append(format_metrics(ebs_write_ops, instance_id))
-
-        ebs_read_ops = get_instance_util(instance_id, "EBSReadOps")
-        metrics_list.append(format_metrics(ebs_read_ops, instance_id))
+    global cloudwatch_client, metrics_list, aws_account_id, aws_account_region
+    aws_account_id = config_file.get("CDK_DEPLOY_ACCOUNT")
+    aws_account_region = config_file.get("CDK_DEPLOY_REGION")
+    cloudwatch_client = boto3.client("cloudwatch", region_name=f"{aws_account_region}")
+    for job_id in job_ids:
+        sub_folder = config["job_configs"][f"{job_id}"].split("/")[5].replace("_split", "").replace(".yaml", "")
+        get_metrics(
+            job_id, ["CPUUtilization", "EBSWriteOps", "EBSReadOps"], s3_bucket, module, benchmark_name, sub_folder
+        )
     results_to_csv()
 
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-app = typer.Typer()
-app.command()(get_hardware_metrics)
-
 
 if __name__ == "__main__":
-    app()
+    get_hardware_metrics()
