63f4db6eb92047ab0357780393e0f3577a1ac93e
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-07-20 10:33:21 -0700

    instance test for get_instance_id

--- tests/unittests/evaluation/hardware_metrics/test_hardware_metrics.py
+++ tests/unittests/evaluation/hardware_metrics/test_hardware_metrics.py
@@ -1,27 +1,51 @@
-import unittest
-from unittest import mock
-from unittest.mock import call
 import os
+import unittest
+from unittest.mock import MagicMock, call, patch
+
 import yaml
 
-PER_DATASET_TEST_CSV_PATH = "random_csv.csv"
-ALL_DATASETS_COMBINED_TEST_CSV_PATH = "random_csv2.csv"
-
-from src.autogluon.bench.eval.hardware_metrics.hardware_metrics import get_metrics, get_hardware_metrics, get_instance_id, get_instance_util, get_job_ids
+from src.autogluon.bench.eval.hardware_metrics.hardware_metrics import (
+    get_hardware_metrics,
+    get_instance_id,
+    get_instance_util,
+    get_job_ids,
+    get_metrics,
+)
 
 test_dir = os.path.dirname(__file__)
-path_to_test_config = os.path.join(test_dir, "test_config.yaml")
+config_file = os.path.join(test_dir, "test_config.yaml")
+if not config_file:
+    raise ValueError("Invalid Config File")
+with open(config_file, "r") as f:
+    config = yaml.safe_load(f)
+
 
 class TestHardwareMetrics(unittest.TestCase):
     def test_get_job_ids(self):
-        config_file = path_to_test_config
-        if not config_file:
-            raise ValueError("Invalid Config File")
-        with open(config_file, "r") as f:
-            config = yaml.safe_load(f)
         job_ids = get_job_ids(config)
-        self.assertEqual(job_ids, ['123456-abc-efg', '010101-xxx-zzz'])
-        
+        self.assertEqual(job_ids, ["123456-abc-efg", "010101-xxx-zzz"])
+
+    @patch("boto3.client")
+    def test_get_instance_id(self, mock_client):
+        job_ids = get_job_ids(config)
+        batch_client, ecs_client = MagicMock(), MagicMock()
+        mock_client.side_effect = [batch_client, ecs_client]
+        mock_batch_response = {
+            "jobs": [
+                {
+                    "container": {
+                        "containerInstanceArn": "abc",
+                        "taskArn": "arn:aws:ecs:us-east-2:123456789:task/agbenchcomputeenvironmen-DhbZ6yaLr_Batch/b3bb44aa78f",
+                    }
+                }
+            ]
+        }
+        batch_client.describe_jobs.return_value = mock_batch_response
+        mock_ecs_response = {"containerInstances": [{"ec2InstanceId": 12345}]}
+        ecs_client.describe_container_instances.return_value = mock_ecs_response
+        instance_id = get_instance_id(job_ids[0])
+        self.assertEqual(instance_id, 12345)
+
 
 if __name__ == "__main__":
     unittest.main()
