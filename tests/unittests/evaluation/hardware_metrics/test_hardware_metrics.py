3af4b7cf2d69b8912b2b8db4a67d164af42684c2
Shreyash Iyengar <shreyash2106@berkeley.edu>
2023-07-20 13:13:41 -0700

    test for get_instance_id completed

--- tests/unittests/evaluation/hardware_metrics/test_hardware_metrics.py
+++ tests/unittests/evaluation/hardware_metrics/test_hardware_metrics.py
@@ -11,6 +11,9 @@ from src.autogluon.bench.eval.hardware_metrics.hardware_metrics import (
     get_job_ids,
     get_metrics,
 )
+from src.autogluon.bench.eval.hardware_metrics import hardware_metrics
+
+
 
 test_dir = os.path.dirname(__file__)
 config_file = os.path.join(test_dir, "test_config.yaml")
@@ -19,6 +22,8 @@ if not config_file:
 with open(config_file, "r") as f:
     config = yaml.safe_load(f)
 
+hardware_metrics.aws_account_id = config["CDK_DEPLOY_ACCOUNT"]
+hardware_metrics.aws_account_region = config["CDK_DEPLOY_REGION"]
 
 class TestHardwareMetrics(unittest.TestCase):
     def test_get_job_ids(self):
@@ -41,11 +46,13 @@ class TestHardwareMetrics(unittest.TestCase):
             ]
         }
         batch_client.describe_jobs.return_value = mock_batch_response
+
         mock_ecs_response = {"containerInstances": [{"ec2InstanceId": 12345}]}
         ecs_client.describe_container_instances.return_value = mock_ecs_response
         instance_id = get_instance_id(job_ids[0])
         self.assertEqual(instance_id, 12345)
-
+        cluster = f"arn:aws:ecs:{hardware_metrics.aws_account_region}:{hardware_metrics.aws_account_id}:cluster/agbenchcomputeenvironmen-DhbZ6yaLr_Batch"
+        ecs_client.describe_container_instances.assert_called_once_with(cluster=cluster, containerInstances=["abc"])
 
 if __name__ == "__main__":
     unittest.main()
